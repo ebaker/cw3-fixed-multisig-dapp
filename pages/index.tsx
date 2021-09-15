2f0660cd1a2ca1c00d699584a8f583a30dd45b49
Eliot Baker <430383+ebaker@users.noreply.github.com>
2021-09-14 22:42:00 -0700

    Add existing & new multisig form to index.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,58 @@
 import type { NextPage } from 'next'
-import Link from 'next/link'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 import WalletLoader from 'components/WalletLoader'
-import { useSigningClient } from 'contexts/cosmwasm'
 
 const Home: NextPage = () => {
-  const { walletAddress } = useSigningClient()
+  const router = useRouter()
+  const [address, setAddress] = useState('')
 
   return (
     <WalletLoader>
-      <h1 className="text-6xl font-bold">
-        Welcome to {process.env.NEXT_PUBLIC_CHAIN_NAME} !
-      </h1>
-
-      <div className="mt-3 text-2xl">
-        Your wallet address is:{' '}
-        <pre className="font-mono break-all whitespace-pre-wrap">
-          {walletAddress}
-        </pre>
-      </div>
-
-      <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 max-w-full sm:w-full">
-        <Link href="/send">
-          <button className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
-            <h3 className="text-2xl font-bold">Send to wallet &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Execute a trasaction to send funds to a wallet address.
-            </p>
-          </button>
-        </Link>
+      <div className="flex flex-col w-full">
+        <div className="grid bg-base-100 place-items-center">
+          <h1 className="text-4xl font-bold mb-8">Existing...</h1>
+          <div className="flex w-full max-w-xl">
+            <div className="relative rounded-full shadow-sm w-full">
+              <input
+                id="multisig-address"
+                className="input input-bordered focus:input-primary input-lg w-full pr-24 rounded-full text-center font-mono text-lg"
+                placeholder="Multisig contract address..."
+                step="0.1"
+                value={address}
+                onKeyDown={(event) => {
+                  if (event.key === 'Enter') {
+                    router.push(`/${event.currentTarget.value}`)
+                  }
+                }}
+                onChange={(event) => setAddress(event.target.value)}
+              />
+              <button
+                className="absolute top-0 right-0 bottom-0 px-8 py-5 rounded-r-full bg-primary text-base-100 text-xl"
+                onClick={() => {
+                  const inputEl = document.getElementById(
+                    'multisig-address'
+                  ) as HTMLInputElement
+                  router.push(`/${inputEl.value}`)
+                }}
+              >
+                GO
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="divider p-8 before:bg-secondary after:bg-secondary before:h-[1px] after:h-[1px]"></div>
+        <div className="flex flex-col items-center">
+          <h1 className="text-4xl font-bold my-8">New...</h1>
+          <div className="w-full max-w-xl">
+            <button
+              className="btn btn-primary btn-lg font-semibold hover:text-base-100 text-2xl rounded-full w-full"
+              onClick={() => router.push('/create')}
+            >
+              + CREATE NEW MULTISIG
+            </button>
+          </div>
+        </div>
       </div>
     </WalletLoader>
   )
